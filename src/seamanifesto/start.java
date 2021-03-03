ef3ffcd6914d22df59d4351c063bdd57d6f979df
debnathsubhradip <debnath.subhradip@gmail.com>
2021-03-03 13:19:07 +0530

    A start UI has been generated. the code in the start.java file will be integrated within the master manager. This will create a more interesting interactive interface for the user.

---
+++ src/seamanifesto/start.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seamanifesto;
+
+/**
+ *
+ * @author alex
+ */
+public class start {
+    start(){
+        
+    }
+    public static void main(String[] args){
+        StartUI start = new StartUI();
+        start.setVisible(true);
+        start.disp();
+    }
+}
