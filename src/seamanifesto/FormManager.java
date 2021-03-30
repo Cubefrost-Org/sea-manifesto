7222dcfee3502ed4c2ae8267fb1a70ba7ca90a0c
Soham Biswas <sohambiswas41@gmail.com>
2021-03-30 19:50:12 +0530

    Form manager (#32)
    * Added Form Manager
    
    * Add form manager

---
+++ src/seamanifesto/FormManager.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seamanifesto;
+/**
+ *
+ * @author nibba
+ */
+public class FormManager {
+    public void createArrivalManifest(){
+        new NewArrivalManifest().setVisible(true);
+    }
+    
+    public void ammendArrivalManifest(){
+        new ArrivalManifestAmmendment().setVisible(true);
+    }
+    
+    public void createDepatureManifest(){
+        new DepartureManifest().setVisible(true);
+    }
+    
+    public void ammendDepartureManifest(){
+        new DepartureManifestAmmendment().setVisible(true);
+    }
+    
+    public void createNotification(){
+        new DepartureNotification().setVisible(true);
+    }
+    
+    public void createEntryInward(){
+        new EntryInward().setVisible(true);
+    }
+}
